5d7fade190b800c6bf6fedce4a887359a940f465
Alejandro Gutiérrez <alejandro_gutierrez.i@outlook.cl>
2020-05-24 22:22:29 -0400

    Se crea "aplicación de consola ASP.Net Core" en Visual Studio 2019
    Al Main se agregan comentarios de lo que se espera de esta aplciación de consola

---
+++ NúmerosPerfectos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NúmerosPerfectos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
