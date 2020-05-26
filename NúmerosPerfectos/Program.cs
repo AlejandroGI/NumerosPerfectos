87c4936c5f66570243feed1b874f4f153df9d04c
Alejandro Gutiérrez <alejandro_gutierrez.i@outlook.cl>
2020-05-25 23:03:47 -0400

    Comentarios extra

--- NúmerosPerfectos/Program.cs
+++ NúmerosPerfectos/Program.cs
@@ -7,10 +7,10 @@
 
 /*
  * SUGERENCIAS DE OPTIMIZACIÓN:
- * -    Los divisores solo llegan hasta la mitad del número, ya que no cabe la mitad de un volumen en otro trozo un poco mayor a dicha mitad. (los divisores solo son hasta la mitád del número)
- * -    Si la operación de suma de sus divisores se pasó del número, el número no es perfecto. Por lo que se puede comenzar la suma desde los últimos divisores
- * -    Si el cuociente de la división es un número entero, se obtienen dos divisores, el divisor y el cuociente (resultado como número entero de la división)
- * -    Los números impares SOLO PUEDEN TENER DIVISORES IMPARES
+ * -    Los divisores solo llegan hasta la mitad del número, ya que no cabe la mitad de un volumen en otro trozo un poco mayor a dicha mitad. (los divisores solo son hasta la mitád del número)        LISTO
+ * -    Si la operación de suma de sus divisores se pasó del número, el número no es perfecto. Por lo que se puede comenzar la suma desde los últimos divisores                                         LISTO
+ * -    Si el cuociente de la división es un número entero, se obtienen dos divisores, el divisor y el cuociente (resultado como número entero de la división)                                          PENDIENTE
+ * -    Los números impares SOLO PUEDEN TENER DIVISORES IMPARES                                                                                                                                         PENDIENTE
  */
 using System;
 using System.Diagnostics;
@@ -21,26 +21,34 @@ namespace NúmerosPerfectos
     {
         static void Main(string[] args)
         {
-            ulong indice;
-            ulong primerDivisor;
+            ulong indice;                                       //Número que se intenta saber si es perfecto
+            ulong divisores;                                    //acumulador de divisores
             Console.WriteLine("CALCULADORA DE NPUMEROS PERFECTOS. Precione Enter para comenzar a calcular");
             Console.ReadLine();
             Stopwatch tiempo = new Stopwatch();
             tiempo.Start();
-            for (indice = 2; indice <= ulong.MaxValue; indice++)
+            for (indice = 2; indice <= ulong.MaxValue; indice++)//buble casi infino empieza del 2 hasta el máximo de ULong
                 {
-                
-                primerDivisor = 1;
-                for (ulong i = indice/2 ; i > 1 ; i--)
-                {   
-                    if (indice % i == 0) primerDivisor += i;
-                    if (primerDivisor> indice) break;
+                divisores = 1;                                  //se asigna 1 al primer divisor
+                for (ulong i = indice/2 ; i > 1 ; i--)          //i es la mitad del índice (para ahorrar vueltas) y se recorre del mayor hasta llegar a 2
+                {
+                    if (indice % i == 0) divisores += i;        //pregunta si el intento de número perfecto es dibicible por el actual índice (partiendo por intento más alto) de ser así se suma a la lista de divisores
+                    if (divisores > indice) break;              //si la actual suma de divisores supera al índice (número actual rompiendo la regla de los perfectos) se rompe la vuelta.
                 }
-                if (primerDivisor == indice)
+                if (divisores == indice)                        //si la suma de divisores da el número, es perfecto
                 {
                     Console.WriteLine($"Numero perfecto : {indice}  | Tiempo demorado en minutos: {tiempo.Elapsed.TotalMinutes}");
                 }
             }
+        }
+        public static ulong NumerosPares(ulong indice, ulong divisores)
+        {
+            return divisores;
+        }
+
+        public static ulong NumerosPrimos(ulong indice, ulong divisores)
+        {
+            return divisores;
         }
     }
 }
